137d795a944360164eed0a2ad25df79d3b65de30
ankush <ankush.patial86@gmail.com>
2015-10-04 22:21:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sp_1/sp_test1/FragmentSPchennai.java
@@ -0,0 +1,7 @@
+package com.example.sp_1.sp_test1;
+
+/**
+ * Created by ankush on 10/4/2015.
+ */
+public class FragmentSPchennai {
+}
---
+++ app/src/main/java/com/example/sp_1/sp_test1/FragmentSPhyderabad.java
@@ -0,0 +1,34 @@
+package com.example.sp_1.sp_test1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+/**
+ * Created by ankush on 10/4/2015.
+ */
+public class FragmentSPhyderabad extends Fragment {
+    @Nullable
+    Spinner  sport_spinner;
+    ArrayAdapter<CharSequence> sport_adapter;
+
+    public FragmentSPhyderabad(){
+
+    }
+        @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+
+
+
+
+
+
+    }
+}
